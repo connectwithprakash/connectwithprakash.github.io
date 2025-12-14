28ba957bf6c493af08904fe3ed7ed07aea087ba1
Prakash Chaudhary <connectwithprakash@gmail.com>
2025-12-13 18:45:22 -0800

    [Fix]: Position featured badge at top-right of project cards
    Move featured badge outside content area and position absolutely
    to keep it fixed at top-right corner regardless of card content.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -58,6 +58,11 @@ const Projects = () => {
                 whileHover={{ y: -8 }}
               >
                 <Link to={`/project/${project.id}`} className="project-card-link">
+                  {project.featured && (
+                    <div className="featured-badge">
+                      <span>Featured</span>
+                    </div>
+                  )}
                   {project.thumbnail && (
                     <div className="project-thumbnail">
                       <img src={project.thumbnail} alt={project.title} />
@@ -68,12 +73,6 @@ const Projects = () => {
                   )}
 
                   <div className="project-content">
-                    {project.featured && (
-                      <div className="featured-badge">
-                        <span>Featured</span>
-                      </div>
-                    )}
-
                     <h3 className="project-title">{project.title}</h3>
                     <p className="project-description">{project.shortDescription}</p>
 
