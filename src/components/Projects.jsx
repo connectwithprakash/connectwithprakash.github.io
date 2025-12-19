78b84e8655879d7ef6f6aff64b270460d96d1635
Prakash Chaudhary <connectwithprakash@gmail.com>
2025-12-18 23:08:13 -0800

    [Fix]: Remove preventDefault that was blocking project card clicks

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -119,7 +119,6 @@ const Projects = () => {
                         className="project-link-btn primary"
                         whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.95 }}
-                        onClick={(e) => e.preventDefault()}
                       >
                         <span>View Details</span>
                         <FaExternalLinkAlt />
